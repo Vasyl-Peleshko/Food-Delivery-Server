1ddcb8a53c7d74b3b7cf33d264ea74ce5fc7b68f
luff3 <vasylpeleshko13@gmail.com>
2025-02-05 10:45:16 +0200

    feat: add creating user functiona;ity with jwt token

---
+++ src/modules/authentication/authentication.module.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { Module } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { AuthenticationController } from './authentication.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/user.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
+  controllers: [AuthenticationController],
+  providers: [AuthenticationService],
+})
+export class AuthenticationModule {}
